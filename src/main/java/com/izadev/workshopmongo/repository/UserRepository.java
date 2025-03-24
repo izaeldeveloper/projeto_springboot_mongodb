a5a7b008ecdcb6a1efd66456616236f586e8cc8f
Izael Araujo <izael.developer@proton.me>
2025-03-24 09:54:28 -0300

    Conectando ao MongoDB com Repository e Service

---
+++ src/main/java/com/izadev/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.izadev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.izadev.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
