eceaa7c0d7246414eaccaca137d2cb25e202c27e
Izael Araujo <izael.developer@proton.me>
2025-03-24 11:18:54 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/izadev/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.izadev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.izadev.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
